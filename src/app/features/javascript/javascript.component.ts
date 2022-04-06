cdead7063a1ac2182ec8153ad415ebdfdefe30ca
Jigar <jigard.bhanushali12@gmail.com>
2022-04-06 10:10:00 +0530

    feat:js concepts

--- src/app/features/javascript/javascript.component.ts
+++ src/app/features/javascript/javascript.component.ts
@@ -41,6 +41,32 @@ export class JavascriptComponent implements OnInit {
         // this.spread()
         // this.stringyfy()
         this.map()
+
+
+
+        //an example object:
+    let myObject = {
+        "male":{
+            "fr":"value",
+            "en":"value2",
+            "es":"value3"
+            },
+        "felame":{
+            "fr":"value",
+            "en":"value2"
+            }
+    };
+//get the part we are looking for:
+    let obj = Object.entries(myObject)
+console.log(typeof(obj))
+//iterate over the object and alert each item:
+    for (let item in obj){
+        console.log(obj[item]);
+        for (let i in obj[item]){
+        console.log(obj[item][i])}
+    }
+
+
     }
 
     array1 = [
@@ -225,7 +251,124 @@ export class JavascriptComponent implements OnInit {
     }
 
 
+js(){
 
+let arr1 = [1,2,3,4,5,6,7]
+let arr2 = [7,8,90,100]
+let arr3 = ['Jigar', 'Hrishi', 'Mehul', 'Jay']
+// Array.prototype.every
+console.log('Array.prototype.every')
+console.log(arr1.every(x => x <= 0))
+
+// Array.prototype.fill
+console.log('Array.prototype.fill')
+console.log(arr1.fill(1,4,6)) 
+
+// Array.prototype.filter
+console.log('Array.prototype.filter')
+console.log(arr2.filter(x => x >= 10))
+
+// Array.prototype.find
+console.log('Array.prototype.find')
+console.log(arr1.find(x => x < 10))
+console.log(arr1.find(x => x > 0))  
+console.log(arr1.find(x => x < 0))
+
+// Array.prototype.findIndex
+console.log('Array.prototype.findIndex')
+console.log(arr1.findIndex(x => x < 10)) 
+console.log(arr1.findIndex(x => x > 0))  
+console.log(arr1.findIndex(x => x < 0))  
+
+// Array.prototype.forEach
+console.log('Array.prototype.forEach')
+arr2.forEach(x => console.log(x))
+// Array.from
+console.log('Array.from')
+console.log(Array.from(arr3, x => x + 1)) 
+console.log(Array.from('Jigar')) 
+console.log(Array.from(arr1, x => x + 7))
+// Array.prototype.includes
+console.log(arr3.includes('Jigar')) 
+console.log(arr3.includes('Jig'))
+
+// Array.prototype.indexOf
+console.log('Array.prototype.indexOf')
+console.log(arr3.indexOf('Raj')) 
+
+// Array.prototype.lastIndexOf
+console.log('Array.prototype.lastIndexOf')
+console.log(arr3.lastIndexOf('Jigar')) 
+// Array.prototype.join
+console.log('Array.prototype.join')
+let arrJoin = ['hello', 'world']
+console.log(arrJoin.join(' '))  
+console.log(arr3.join())        
+
+// Array.prototype.keys
+console.log('Array.prototype.keys')
+let arr = ['a', , 'c'];
+let keys = Object.keys(arr3)
+let arr3Keys = arr3.keys()
+console.log(keys)
+console.log(arr3Keys)
+
+// Array.prototype.map
+console.log('Array.prototype.map')
+console.log(arr2.map(x => x + 2)) 
+
+// Array.prototype.reduce
+console.log('Array.prototype.reduce')
+console.log(arr2.reduce((x, y) => {
+  console.log('currentValue of x:', x)
+  console.log('currentValue of y:', y)
+  return x + y
+}))
+
+// Array.prototype.reverse
+console.log('Array.prototype.reverse')
+console.log(arr2.reverse())
+console.log(arr2)
+
+// Array.prototype.shift
+console.log('Array.prototype.shift')
+console.log(arr2.shift()) 
+console.log(arr2);
+
+// Array.prototype.slice
+console.log('Array.prototype.slice')
+console.log(arr2.slice(0,1))
+console.log(arr2) 
+
+// Array.prototype.some
+console.log('Array.prototype.some')
+console.log(arr2.some(x => x > 0))
+
+// Array.prototype.sort
+console.log('Array.prototype.sort')
+console.log(arr2.sort()) 
+console.log(arr2) 
+
+// Array.prototype.splice
+console.log('Array.prototype.splice')
+console.log(arr3.splice(3,1)) 
+console.log(arr3)
+
+// Array.prototype.unshift
+console.log('Array.prototype.unshft')
+console.log(arr2.unshift(3,4))
+console.log(arr2); 
+
+const obj = {
+    name: 'Jigar',
+    rank: '2'
+  };
+  
+  Object.entries(obj).forEach(entry => {
+    const [key, value] = entry;
+    console.log(key, value);
+  });
+}
 
 
 }
